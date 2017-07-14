6d1ae5d75430814c6c77f28ac66e2dda04e2ed7c
njdaniel <nicholasjdaniel@gmail.com>
2017-07-14 17:50:14 -0400

    if example

---
+++ if/if_example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if true {
+		fmt.Println("This will run")
+	}
+	if !false{
+		fmt.Println("This will also run")
+	}
+	if false {
+		fmt.Println("This will not run")
+	}
+}
