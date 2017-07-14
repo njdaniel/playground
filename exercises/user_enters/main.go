a84236e893dfce477ee77ae048378c9b0dacc64f
njdaniel <nicholasjdaniel@gmail.com>
2017-07-14 18:04:25 -0400

    small calc

---
+++ exercises/user_enters/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numOne int
+	var numTwo int
+	fmt.Print("Please enter a large number: ")
+	fmt.Scan(&numOne)
+	fmt.Print("Please enter a small number: ")
+	fmt.Scan(&numTwo)
+	fmt.Println(numOne, "/", numTwo, "=", float32(numOne) / float32(numTwo))
+}
