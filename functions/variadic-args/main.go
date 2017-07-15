8ce7684026aa439bd918595fdea2ca5dc0856ac0
nicho <nicholasjdaniel@gmail.com>
2017-07-15 15:36:48 -0700

    deconstruct args

---
+++ functions/variadic-args/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	data := []float64{43, 56, 76, 14}
+	n := average(data...)
+	fmt.Println(n)
+}
+
+func average(sf ...float64) float64  {
+	total := 0.0
+	for _, v := range sf {
+		total += v
+	}
+	return total / float64(len(sf))
+}
