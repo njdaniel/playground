b91002c5f435481a2fa14cb1eadf905dfebe37be
njdaniel <nicholasjdaniel@gmail.com>
2017-09-19 15:04:06 -0400

    methods

--- structs/methods/main.go
+++ structs/methods/main.go
@@ -1,4 +1,18 @@
 package main
 
+import "fmt"
+
+type Person struct {
+	first string
+	last string
+	age int
+}
+
+func (p Person) fullName() string {
+	return p.first + p.last
+}
+
 func main() {
+	p1 := Person{"Nick", "Daniel", 27}
+	fmt.Println(p1.fullName())
 }
