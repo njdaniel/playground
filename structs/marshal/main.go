174c63d60f4cf3402f8752c173ca0943d75fc3f5
njdaniel <nicholasjdaniel@gmail.com>
2017-09-19 14:59:35 -0400

    struct marshal json

--- structs/marshal/main.go
+++ structs/marshal/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	First string
+	Last string
+	Age int
+	notExported int
+}
+
 func main() {
+	p1 := Person{"Nick", "Daniel", 27, 000}
+	bs, _ := json.Marshal(p1)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+	fmt.Println(bs)
 }
