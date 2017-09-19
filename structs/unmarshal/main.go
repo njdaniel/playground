e02110a78a5979f85cffffc7f90a45cfba41d40d
njdaniel <nicholasjdaniel@gmail.com>
2017-09-19 15:46:37 -0400

    unmarshal struct

--- structs/unmarshal/main.go
+++ structs/unmarshal/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"encoding/json"
+)
+
+type Person struct {
+	First string
+	Last string
+	Age int
+}
+
 func main() {
+	var p1 Person
+	fmt.Println(p1.First)
+	fmt.Println(p1.Last)
+	fmt.Println(p1.Age)
+
+	bs := []byte(`{"First": "Nick", "Last": "Danie;", "Age": 27}`)
+	json.Unmarshal(bs, &p1)
+
+	fmt.Println("----------------")
+	fmt.Println(p1.First)
+	fmt.Println(p1.Last)
+	fmt.Println(p1.Age)
+	fmt.Printf("%T \n ", p1)
 }
