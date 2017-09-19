24591720e6dbb75dee6c65541b0377b46f04bec2
njdaniel <nicholasjdaniel@gmail.com>
2017-09-19 15:19:08 -0400

    tags

--- structs/tags/main.go
+++ structs/tags/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"encoding/json"
+)
+
+type Person struct {
+	First string
+	Last string
+	Age int `json:"wisdom score"`
+}
+
 func main() {
+	var p1 Person
+	fmt.Println(p1.First)
+	fmt.Println(p1.Last)
+	fmt.Println(p1.Age)
+
+	bs := []byte(`{"First": "Nick", "Last": "Daniel", "wisdom score": 27}`)
+	json.Unmarshal(bs, &p1)
+
+	fmt.Println("-----------------")
+	fmt.Println(p1.First)
+	fmt.Println(p1.Last)
+	fmt.Println(p1.Age)
+	fmt.Printf("%T \n", p1)
 }
