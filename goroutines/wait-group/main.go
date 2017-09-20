0ebc29ea44de148ef8d2d6b2c63e5dcc587bcaf2
njdaniel <nicholasjdaniel@gmail.com>
2017-09-20 17:07:59 -0400

    wait group

---
+++ goroutines/wait-group/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2)
+	go foo()
+	go bar()
+	wg.Wait()
+}
+
+func foo()  {
+	for i := 0; i < 45; i++ {
+		fmt.Println("Foo:", i)
+	}
+	wg.Done()
+}
+
+func bar()  {
+	for i := 0; i < 45; i++ {
+		for i := 0; i < 45; i++ {
+			fmt.Println("Bar:", i)
+		}
+		wg.Done()
+	}
+}
