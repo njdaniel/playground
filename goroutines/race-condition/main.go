f7a5fc57a15eeefa8ac1d76287a665c3d2a33e89
njdaniel <nicholasjdaniel@gmail.com>
2017-09-20 17:02:28 -0400

    race condition

---
+++ goroutines/race-condition/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"sync"
+	"time"
+	"math/rand"
+	"fmt"
+)
+
+var wg sync.WaitGroup
+var counter int
+
+func incrementor(s string)  {
+	for i := 0; i < 20; i++ {
+		x := counter
+		x++
+		time.Sleep(time.Duration(rand.Intn(3)) * time.Millisecond)
+		counter = x
+		fmt.Println(s, i, "Counter:", counter)
+	}
+	wg.Done()
+}
+
+func main() {
+	wg.Add(2)
+	go incrementor("Foo:")
+	go incrementor("Bar:")
+	wg.Wait()
+	fmt.Println("Final Counter:", counter)
+}
