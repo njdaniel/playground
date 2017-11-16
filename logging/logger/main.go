e966aec4e57cfd760269fcfc983cfec95de9632e
njdaniel <nicholasjdaniel@gmail.com>
2017-11-16 17:19:13 -0500

    logging

---
+++ logging/logger/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+var  (
+	Error *log.Logger
+	Debug *log.Logger
+	Info *log.Logger
+)
+
+func init()  {
+	f := log.LstdFlags | log.Lshortfile
+	Error = log.New(os.Stderr, "Error ", f)
+	Debug = log.New(os.Stdout, "Debug ", f)
+	Info = log.New(os.Stdout, "Info ", f)
+}
+
+func main() {
+	timeNow := time.Now()
+	Info.Println(timeNow)
+
+}
