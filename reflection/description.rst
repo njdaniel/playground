938ed3dba6a9c4d1c5c5ee683a6b1d670302ac8a
nick <nicholasjdaniel@gmail.com>
2018-01-09 16:31:57 -0500

    add go reflection notes

---
+++ reflection/description.rst
@@ -0,0 +1,15 @@
+Reflection in software development refers to the program's ability to examine its own structure.
+
+Or use code generation that is code that writes code. Also called metaprogramming.
+Other languages use generics to accomplish this.
+
+Three features of Reflection:
+ Examine objects during runtime. Determine if object satisfies an interface, underlying kind,
+walk over fields and modify data.
+Located in the 'reflect' package
+1) Values - the data itself
+2) Types - the type of the data
+3) Kinds - struct, ptr, int, float64, string, slice, func, etc
+
+Switching based on type and kind:
+
---
+++ reflection/main.go
@@ -0,0 +1,40 @@
+/*
+Take generic values(interfaces{}) and do something useful based on underlying type
+ */
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var a uint8 = 2
+	var b int = 37
+	var c string = "3.2"
+	res := sum(a, b, c)
+	fmt.Printf("Result: %f\n", res)
+}
+
+func sum(v ...interface{}) float64 {
+	var res float64 = 0
+	for _, val := range v {
+		switch val.(type) {
+		case int:
+			res += float64(val.(int))
+		case int64:
+			res += float64(val.(int64))
+		case uint8:
+			res += float64(val.(uint8))
+		case string:
+			a, err := strconv.ParseFloat(val.(string), 64)
+			if err != nil {
+				panic(err)
+			}
+			res += a
+		default:
+			fmt.Printf("Unsupported type %T. Ignoring. \n", val)
+		}
+	}
+	return res
+}
