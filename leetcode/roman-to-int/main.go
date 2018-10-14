6a22c493d09591149f1c3b87932653883814b867
nick <nicholasjdaniel@gmail.com>
2018-10-14 12:50:57 -0400

    Add leetcode romantoint problem

---
+++ leetcode/roman-to-int/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	
+}
+
+func romanToInt(s string) int {
+	converts := map[string]int {
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+	runes := []rune(s)
+	sum := 0
+	for i, j := range(runes) {
+
+	}
+
+}
