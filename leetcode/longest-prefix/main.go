826768c1e2ab79b991a5fcbe505bed8fa693bcc3
nick <nicholasjdaniel@gmail.com>
2018-10-15 22:29:55 -0400

    Add longestcommonprefix testing

--- leetcode/longest-prefix/main.go
+++ leetcode/longest-prefix/main.go
@@ -1,9 +1,13 @@
 package main
 
+import (
+	"strings"
+)
+
 func main() {
 	
 }
 
 func longestCommonPrefix(strs []string) string {
-
+	return strings.Join(strs,",")
 }
---
+++ leetcode/longest-prefix/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestlongestCommonPrefix(t *testing.T)  {
+	input := []string{"flower", "flow", "flight"}
+	result := longestCommonPrefix(input)
+	if result != "fl" {
+		t.Errorf(`longestCommonPrefix(%s) == %s. Should be "fl" `, strings.Join(input,","), result)
+	}
+	input2 := []string{"dog","racecar","car"}
+	result2 := longestCommonPrefix(input)
+	if result2 != "" {
+		t.Errorf(`longestCommonPrefix(%s) == %s. Should be "" `, strings.Join(input2,","), result2)
+	}
+}
