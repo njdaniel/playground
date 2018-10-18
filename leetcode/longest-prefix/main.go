03fa9684836e3c350318db210abe6299d33c6eb8
nick <nicholasjdaniel@gmail.com>
2018-10-17 20:56:00 -0400

    Add test to main

--- leetcode/longest-prefix/main.go
+++ leetcode/longest-prefix/main.go
@@ -1,14 +1,18 @@
 package main
 
+import "fmt"
 
 func main() {
-	
+	input := []string{"flower", "flow", "flight"}
+	fmt.Println(longestCommonPrefix(input))
 }
 
 func longestCommonPrefix(strs []string) string {
 	prefix := make([]rune,0)
 	for i, j := range strs {
-		for m, n := range j {
+
+		if j[i] != j[i]-1 {
+
 		}
 	}
 	return string(prefix)
