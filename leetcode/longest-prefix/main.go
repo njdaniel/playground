877a4314bd4ac41d0310614d14f7929a55e8b0e8
nick <nicholasjdaniel@gmail.com>
2018-10-19 11:40:12 -0400

    Complete finding longest prefix

--- leetcode/longest-prefix/main.go
+++ leetcode/longest-prefix/main.go
@@ -9,18 +9,28 @@ func main() {
 
 func longestCommonPrefix(strs []string) string {
 	prefix := make([]rune,0)
-	var s1 string
-	//for i, j := range strs {
-	//	if i == 0 {
-	//		s1 = j
-	//	}
-	//	if j[i] != j[i]-1 {
-	//
-	//	}
-	//}
-	 for i := 0; i < len(strs); i++ {
 
-	 }
+	var min = func (x,y int) int {
+		if x < y {
+			return x
+		}
+		return y
+	}
+	v1 := []rune(strs[0])
+	v2 := []rune(strs[1])
+	for i := 0; i < len(strs); i++ {
+		for j := 0; j < min(len(v1),len(v2)); j++ {
+			if v1[j] == v2[j] {
+				prefix = append(prefix, v1[j])
+			}
+		}
+		v1 = prefix
+		prefix = nil
+		if (i+2) < len(strs) {
+			v2 = []rune(strs[i+2])
+		}
 
-	return string(prefix)
+	}
+
+	return string(v1)
 }
