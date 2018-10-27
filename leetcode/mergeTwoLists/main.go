118f478b44ccd57c7f30de938357dadc5c078315
nick <nicholasjdaniel@gmail.com>
2018-10-27 18:05:40 -0400

    Add sortlinklist

---
+++ leetcode/mergeTwoLists/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	
+}
+
+
+ //Definition for singly-linked list.
+ type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
