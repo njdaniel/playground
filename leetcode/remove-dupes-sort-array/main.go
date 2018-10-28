dfbb6a2644786fb90849461ae910e3ea7b0693b9
nick <nicholasjdaniel@gmail.com>
2018-10-28 11:50:06 -0400

    Fix removeDupesFromSortArray   One off

--- leetcode/remove-dupes-sort-array/main.go
+++ leetcode/remove-dupes-sort-array/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	n := []int{0,0,1,1,1,2,2,3,3,4}
+	//n := []int{0,0,1,1,1,2,2,3,3,4}
+	n := []int{1,1,2}
 	fmt.Println(removeDuplicates(n))
 }
 
 func removeDuplicates(nums []int) int {
-	for i := 0; i < len(nums)-2; {
+	for i := 0; i < len(nums)-1; {
 		if nums[i] == nums[i+1] {
 			nums = append(nums[:i], nums[i+1:]...)
 		} else {
