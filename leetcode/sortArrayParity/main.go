5c83720df07cf5b1bb6d3a13ad9454af9ba83b97
nick <nicholasjdaniel@gmail.com>
2018-11-04 15:32:06 -0500

    Add sortArrayByParity

---
+++ leetcode/sortArrayParity/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := []int{3,1,2,4}
+	fmt.Println(sortArrayByParity(input))
+}
+
+func sortArrayByParity(A []int) []int {
+	//return evens then odds
+	even := make([]int, 0)
+	odd := make([]int, 0)
+	B := make([]int, 0)
+	for _, v := range A {
+		if v % 2 == 0 {
+			even = append(even, v)
+		} else {
+			odd = append(odd, v)
+		}
+	}
+	B = append(even, odd...)
+	return B
+}
