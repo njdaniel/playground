d718cbcf2d99174ce7a05b67ffcb35c3fe5fec44
nick <nicholasjdaniel@gmail.com>
2018-11-04 15:57:48 -0500

    Add judgeCircle

---
+++ leetcode/judgeCircle/main.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	
+}
+
+//judgeCircle does the robot end up at the start
+func judgeCircle(moves string) bool {
+	//cmds: 
+	//  L: left
+	//  R: right
+	//  U: up
+	//  D: down
+	//LR and UD cancel each other out
+	ms := []rune(moves)
+	u := 0
+	d := 0
+	l := 0
+	r := 0
+	for _, m := range ms {
+		switch m {
+		case 'U':
+			u++
+		case 'D':
+			d++
+		case 'L':
+			l++
+		case 'R':
+			r++
+		}
+	}
+	if u == d && l == r {
+		return true
+	}
+	return false
+}
