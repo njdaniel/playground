f103b6204742436ef8cb56f598b911427942e629
nicholasdaniel <nicholasjdaniel@gmail.com>
2018-12-20 21:11:10 -0500

    Finish stock profit leetcode

--- leetcode/stock/main.go
+++ leetcode/stock/main.go
@@ -1,9 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	input := []int{7,1,5,3,6,4}
+	fmt.Println(maxProfit(input))
 }
 
 func maxProfit(prices []int) int {
 	// find the greatest difference between
+	// loop through compare with rest
+	profit := 0
+	for k, v := range prices {
+		for i := k; i < len(prices); i++ {
+			if prices[i] - v > profit {
+				profit = prices[i] - v
+			}
+		}
+	}
+	return profit
 }
