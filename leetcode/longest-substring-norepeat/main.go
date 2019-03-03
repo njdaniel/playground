30d83378727b9dbf1eea96b1f25681252fabb834
nick <nicholasjdaniel@gmail.com>
2019-03-03 11:11:57 -0500

    Changes to long sub

--- leetcode/longest-substring-norepeat/main.go
+++ leetcode/longest-substring-norepeat/main.go
@@ -14,7 +14,7 @@ func lengthOfLongestSubstring(s string) int {
 	var m = make(map[rune]rune, 0)
 	longest := 0
 	//loop through s put rune in map,
-	for num, r := range s {
+	for idx, r := range s {
 		_, ok := m[r]
 		if ok {
 			if len(m) > longest {
@@ -25,23 +25,43 @@ func lengthOfLongestSubstring(s string) int {
 			// }
 			//remove (last ref to r)->0
 			lastr := false
-			for i := num - 1; i >= 0; i-- {
+			lastrIdx := 0
+			for i := idx - 1; i >= 0; i-- {
 				// fmt.Printf("%T", s[i])
 				if rune(s[i]) == r {
 					lastr = true
+					lastrIdx = i
 				}
 				if lastr {
 					key := rune(s[i])
-					delete(m, key)
+					dupe := false
+					for j := lastrIdx + 1; j <= len(s)-1; j++ {
+						if rune(s[j]) == key {
+							dupe = true
+						}
+					}
+					if !dupe {
+						delete(m, key)
+					}
 				}
 			}
 			m[r] = r
+			// for _, sub := range m {
+			// 	fmt.Printf("%s", string(m[sub]))
+			// }
+			if len(m) > longest {
+				longest = len(m)
+			}
 		} else {
 			m[r] = r
+			// fmt.Println(string(r))
 			if len(m) > longest {
 				longest = len(m)
 			}
 		}
 	}
+	// for _, sub := range m {
+	// 	fmt.Printf("%s", string(m[sub]))
+	// }
 	return longest
 }
