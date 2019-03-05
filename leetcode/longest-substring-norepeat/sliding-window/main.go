fc77a12e20a982433954e3063342de8f98f07385
nick <nicholasjdaniel@gmail.com>
2019-03-04 23:41:13 -0500

    submit

--- leetcode/longest-substring-norepeat/sliding-window/main.go
+++ leetcode/longest-substring-norepeat/sliding-window/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	nput := "anasdlkfj"
+	nput := "pwwkew"
 	fmt.Println(lengthOfLongestSubstring(nput))
 }
 
@@ -13,20 +13,19 @@ func lengthOfLongestSubstring(s string) int {
 	//first attempt was with brute force
 	//it was slow and a little messy to
 	longest := 0
-
 	//each char has a index
 	var m = make(map[rune]int, 0)
-	for i, j := 0, 0; j <= len(s)-1; j++ {
+	for i, j := 0, 0; j <= len(s)-1 && i <= len(s); {
 		if _, ok := m[rune(s[j])]; !ok {
 			m[rune(s[j])] = j
+			if j-i+1 > longest {
+				longest = j - i + 1
+			}
+			j++
 		} else {
 			delete(m, rune(s[j]))
 			i++
 		}
-		if j-i+1 > longest {
-			longest = j - i + 1
-		}
-
 	}
 	return longest
 }
