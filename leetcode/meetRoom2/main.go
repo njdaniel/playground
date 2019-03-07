a075379710c032976467eb71049f05ae669e3374
nick <nicholasjdaniel@gmail.com>
2019-03-06 20:54:32 -0500

    Attempted fix

--- leetcode/meetRoom2/main.go
+++ leetcode/meetRoom2/main.go
@@ -7,18 +7,26 @@ import (
 func main() {
 	intervals := make([]Interval, 0)
 	i1 := Interval{
-		Start: 0,
-		End:   30,
+		Start: 20,
+		End:   45,
 	}
 	i2 := Interval{
-		Start: 5,
-		End:   10,
+		Start: 12,
+		End:   12,
 	}
 	i3 := Interval{
-		Start: 15,
+		Start: 2,
+		End:   50,
+	}
+	i4 := Interval{
+		Start: 14,
 		End:   20,
 	}
-	intervals = append(intervals, i1, i2, i3)
+	i5 := Interval{
+		Start: 3,
+		End:   5,
+	}
+	intervals = append(intervals, i1, i2, i3, i4, i5)
 	fmt.Println(minMeetingRooms(intervals))
 }
 
@@ -34,16 +42,29 @@ func minMeetingRooms(intervals []Interval) int {
 	rooms := make([][]int, 0)
 	for _, i := range intervals {
 		addRoom := true
-		for _, r := range rooms {
-			if i.Start > r[0] && i.End > r[len(r)-1] {
-				r = append(r, i.Start, i.End)
+		for k, r := range rooms {
+			if i.Start >= r[len(r)-1] && i.End > r[len(r)-1] {
+				rooms[k] = append(r, i.Start, i.End)
 				addRoom = false
 				break
-			} else if i.Start < r[0] && i.End < r[len(r)-1] {
+			} else if i.Start < r[0] && i.End <= r[0] {
 				is := []int{i.Start, i.End}
-				r = append(is, r...)
+				rooms[k] = append(is, r...)
 				addRoom = false
 				break
+			} else if len(r) >= 4 {
+				j := 1
+				for ; j < len(r)/2; j++ {
+					if i.Start >= r[1+(j-1)*2] && i.End <= r[2+(j-1)*2] {
+						is := []int{i.Start, i.End}
+						rooms[k] = append(r[:2+(j-1)*2], append(is, r[2+(j-1)*2:]...)...)
+						addRoom = false
+						break
+					}
+					if !addRoom {
+						break
+					}
+				}
 			}
 		}
 		if addRoom {
