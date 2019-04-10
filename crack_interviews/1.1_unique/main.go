b6c4ff4c4b583cb6572a76d87f004aa1f9644a38
nick <nicholasjdaniel@gmail.com>
2019-04-09 22:47:41 -0400

    add unique with no additional data structure

--- crack_interviews/1.1_unique/main.go
+++ crack_interviews/1.1_unique/main.go
@@ -4,7 +4,7 @@ func main() {
 
 }
 
-//IsUnique detemine if string contains all characters
+//IsUnique detemine if string contains all unique characters
 func IsUnique(s string) bool {
 	u := make(map[rune]rune)
 	for _, char := range s {
@@ -14,5 +14,13 @@ func IsUnique(s string) bool {
 			return false
 		}
 	}
+	return true
+}
+
+// IsUnique2 determine if string contains all unique char but no additional data structures
+func IsUnique2(s string) bool {
+	//assume not empty
+	//sort
+	//have two pointers compare if same return false otherwise true
 	return true
 }
