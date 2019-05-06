0477864c008487572e4dc5152b63d10436f58e73
nichdani <nichdani@cisco.com>
2019-05-06 18:34:49 -0400

    add count

--- leetcode/binaryTree/main.go
+++ leetcode/binaryTree/main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-	root := TreeNode{5, nil, nil}
+	l1 := &TreeNode{1, nil, nil}
+	root := TreeNode{5, l1, nil}
 	c := countUnivalSubtrees(&root)
 	fmt.Println(c)
 }
@@ -18,17 +19,21 @@ type TreeNode struct {
 }
 
 func countUnivalSubtrees(root *TreeNode) int {
-	var count int
-
+	count := 0
+	_, count = isUni(root, count)
 	return count
 }
 
-func isUni(n *TreeNode) bool {
+func isUni(n *TreeNode, count int) (bool, int) {
 	if n.Left == nil && n.Right == nil {
-		return true
+		count++
+		return true, count
 	}
-	if ((isUni(n.Left) && n.Val == n.Left.Val) || n.Left == nil) && ((isUni(n.Right) && n.Val == n.Right.Val) || n.Right == nil) {
-		return true
+	l, _ := isUni(n.Left, count)
+	r, _ := isUni(n.Right, count)
+	if ((l == true && n.Val == n.Left.Val) || n.Left == nil) && ((r == true && n.Val == n.Right.Val) || n.Right == nil) {
+		count++
+		return true, count
 	}
-	return false
+	return false, count
 }
