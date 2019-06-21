92c2cc603c094f3dd29fa3ac1d7613a6ca7e1d09
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-06-21 09:14:07 -0400

    change ptr to index

--- leetcode/maxSubArray/main.go
+++ leetcode/maxSubArray/main.go
@@ -9,45 +9,47 @@ func main() {
 }
 
 func maxSubArray(nums []int) int {
-	var f *int
-	var e *int
+	var f int
+	var e int
 	//f = &nums[0]
 	//e = &nums[0]
 	for x := 0; x < len(nums); x++ {
-		if f == nil && e == nil {
-			f = &nums[x]
-			e = &nums[x]
-			fmt.Println("init")
-			continue
-		}
+		//if f == nil && e == nil {
+		//	f = x
+		//	e = x
+		//	fmt.Println("init")
+		//	continue
+		//}
 
-		fmt.Printf("front pointer %d\n", *f)
+
 		//should front move up?
 		vf := 0
-		for y := *f; y <= x; y++ {
+		for y := f; y <= x; y++ {
 			vf += nums[y]
 		}
 		if vf > 0 {
-			f = &nums[x]
+			f = x
 		}
 
 		//should end move up?
 		ve := 0
-		for y := *e; y <= x || y <= *f; y++ {
+		for y := e; y <= x || y <= f; y++ {
 			ve += nums[y]
 		}
 		if ve < 0 {
-			e = &nums[x]
+			e = x
 		}
 		sum := 0
-		for y := *e; y <= *f; y++ {
+		for y := e; y <= f; y++ {
 			sum += nums[y]
 		}
-		fmt.Println(sum)
+		fmt.Printf("front pointer %d\n", f)
+		fmt.Printf("end pointer %d\n", e)
+		fmt.Printf("round %d sum: %d\n\n", x, sum)
 	}
 
 	sum := 0
-	for y := *e; y <= *f; y++ {
+	for y := e; y <= f; y++ {
 		sum += nums[y]
 	}
 	return sum
