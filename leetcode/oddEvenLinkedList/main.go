b6e4a71612c3d2722a0e62481e64b88722e82710
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-06-23 20:12:36 -0400

    add oddeven func

--- leetcode/oddEvenLinkedList/main.go
+++ leetcode/oddEvenLinkedList/main.go
@@ -1,5 +1,43 @@
 package main
 
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
 func main() {
-	$END$
+	
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+	//check if
+	odd := head
+	evenHead := head.Next
+	even := odd.Next
+	p := evenHead.Next
+	counter := 3
+
+	for ;p != nil; counter++{
+		switch {
+		case counter % 2 == 1 :
+			odd.Next = p
+			odd = p
+			p = p.Next
+		case counter % 2 == 0:
+			even.Next = p
+			even = p
+			p = p.Next
+		}
+	}
+
+	odd.Next = evenHead
+
+	return head
 }
