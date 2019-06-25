4d7ee0d8363baa267aa70ba8783198ee6dce91c8
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-06-25 10:22:04 -0400

    add word pattern match test

---
+++ leetcode/wordPattern/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_wordPattern(t *testing.T) {
+	type args struct {
+		pattern string
+		str     string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"ex1", args{"abab", "cat dog cat dog"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := wordPattern(tt.args.pattern, tt.args.str); got != tt.want {
+				t.Errorf("wordPattern() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
