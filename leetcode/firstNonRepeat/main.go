145b8fd8a560459758585aa0c6e9404de1cbe56f
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-06-25 09:33:04 -0400

    pass default for unique

--- leetcode/firstNonRepeat/main.go
+++ leetcode/firstNonRepeat/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	$END$
+	input := "abzabc"
+	fmt.Println(firstUniqChar(input))
+}
+
+
+func firstUniqChar(s string) int {
+	m := make(map[rune][]int, 0)
+	u := -1
+	//ss := make([]int, 0)
+	// loop string and add to m and ss
+	//when adding check if exist, remove non-unique from ss
+	for i, r := range s {
+		if _, ok := m[r]; !ok {
+			m[r] = []int{i,}
+			//ss = append(ss, i)
+		} else {
+			m[r] = append(m[r], i)
+		}
+	}
+	for _, v := range m {
+		if len(v) == 1 && ((v[0]<u && u != -1)|| u ==-1 ){
+			u = v[0]
+		}
+	}
+
+	return u
 }
