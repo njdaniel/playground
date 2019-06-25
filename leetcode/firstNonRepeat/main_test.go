0fa3b9778e2e2b63f489e49e78b13644c848e8e5
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-06-25 09:37:55 -0400

    add tests for first unique

---
+++ leetcode/firstNonRepeat/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func Test_firstUniqChar(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"ex1", args{"abzabc"}, 2},
+		{"ex2", args{"leetcode"}, 0},
+		{"ex3", args{"loveleetcode"}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstUniqChar(tt.args.s); got != tt.want {
+				t.Errorf("firstUniqChar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
