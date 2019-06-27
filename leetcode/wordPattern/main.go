ad0b187733d2cc47b75c64fd4f13c292e4788f9a
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-06-27 18:00:54 -0400

    fix word pattern

--- leetcode/wordPattern/main.go
+++ leetcode/wordPattern/main.go
@@ -17,15 +17,21 @@ func wordPattern(pattern string, str string) bool {
 	//ss := make([]string, 0)
 	ss := strings.Split(str, " ")
 	fmt.Printf("len ss: %d\n", len(ss))
-	m := make(map[rune]string)
+	m := make(map[string]rune)
+	m2 := make(map[rune]string)
 	pr := []rune(pattern)
 	if len(pr) != len(ss) {
 		return false
 	}
 	for i := 0; i < len(pr); i++ {
-		if _, ok := m[pr[i]]; !ok {
-			m[pr[i]] = ss[i]
-		} else if ok && m[pr[i]] != ss[i]{
+		_, ok := m[ss[i]]
+		_, ok2 := m2[pr[i]]
+		if !ok && !ok2 {
+			m[ss[i]] = pr[i]
+			m2[pr[i]] = ss[i]
+		} else if ok && ok2 && m[ss[i]] == pr[i]{
+			continue
+		} else {
 			return false
 		}
 	}
