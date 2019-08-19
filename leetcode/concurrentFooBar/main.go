489d34560586311fc96bb16b2a67511e86a69f52
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-08-19 11:08:27 -0400

    add concurrent issue

---
+++ leetcode/concurrentFooBar/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// printing foo and bar in order with goroutines
+func main() {
+	
+}
+
+func foo()  {
+	fmt.Printf("foo")
+}
+
+func bar()  {
+	fmt.Printf("bar")
+}
