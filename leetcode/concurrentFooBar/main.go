960847919ea0a293bd445793935c6486d616cab6
nick <nicholasjdaniel@gmail.com>
2019-08-20 13:21:51 -0400

    print foobar concurrent

--- leetcode/concurrentFooBar/main.go
+++ leetcode/concurrentFooBar/main.go
@@ -1,16 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 // printing foo and bar in order with goroutines
 func main() {
-	
+	fmt.Println("starting..")
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		go bar()
+		go foo()
+		wg.Wait()
+	}
+
 }
 
 func foo()  {
 	fmt.Printf("foo")
+	wg.Done()
 }
 
 func bar()  {
-	fmt.Printf("bar")
+	fmt.Printf("bar\n")
+	wg.Done()
 }
--- map/main.go
+++ map/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	m := make(map[string][]int)
+	m["tiger"] = []int{3}
+	t := "lion"
+	if _, ok := m[t]; ok {
+		fmt.Println(m[t])
+	} else {
+		m[t] = append(m[t], 4)
+		fmt.Println(m[t])
+	}
+
+	for _, v := range m[t] {
+		switch v {
+		case 4:
+			fmt.Println("just 4")
+		default:
+			fmt.Println("sjhit")
+
+		}
+	}
+
+	for k, meep := range m {
+		fmt.Printf("%s: ",k)
+		fmt.Println(meep)
+	}
 }
