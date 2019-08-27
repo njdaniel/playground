162988d852db3b620dd98898af8164f4f347d1a2
nick <nicholasjdaniel@gmail.com>
2019-08-27 17:43:48 -0400

    add counter with range

---
+++ gopl.io/ch8/pipeline2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main()  {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	//counter
+	go func() {
+		for x := 0; x < 10; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+
+	//squarer
+	go func() {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	//printer
+	for x := range squares {
+		fmt.Println(x)
+	}
+}
