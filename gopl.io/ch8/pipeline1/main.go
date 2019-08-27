9c4264918fbe402329702bb782d95ce57ec68691
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-08-26 22:03:12 -0400

    add pipeline

---
+++ gopl.io/ch8/pipeline1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	//counter
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	//squarer
+	go func() {
+		for {
+			x := <-naturals
+			squares <- x * x
+		}
+	}()
+
+	//printer
+	for {
+		fmt.Println(<-squares)
+	}
+	
+}
