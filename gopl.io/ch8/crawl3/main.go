16a2f4e086156a5dd301cfe80f384d010994ad5a
nick <nicholasjdaniel@gmail.com>
2019-08-31 22:54:37 -0400

    create 20 concurrent https connections

--- gopl.io/ch8/crawl3/main.go
+++ gopl.io/ch8/crawl3/main.go
@@ -1,6 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"playground/gopl.io/ch5/links"
+)
+
 //alternate to crawl2 is to use just 20https requests concurrently
 func main() {
-	worklist := make(chan []string)
+	worklist := make(chan []string)  //list of urls, may have dupes
+	unseenLinks := make(chan string) //de-dupe
+
+	//add cl args to worklist
+	go func() { worklist <- os.Args[1:] }()
+
+	//create 20 crawlers goroutines to fetch each unseen link
+	for i := 0; i < 20; i++ {
+		go func() {
+			for link := range unseenLinks {
+				foundLinks := crawl(link)
+				go func() { worklist <- foundLinks }()
+			}
+		}()
+	}
+}
+
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{} // send blank to acquire token space
+	list, err := links.Extract(url)
+	<-tokens
+	if err != nil {
+		log.Print(err)
+	}
+	return list
 }
