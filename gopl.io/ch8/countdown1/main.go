e16691a47b73e8e774fcd2322ead3e9f7b2791fc
nick <nicholasjdaniel@gmail.com>
2019-09-01 19:30:21 -0400

    add abort

--- gopl.io/ch8/countdown1/main.go
+++ gopl.io/ch8/countdown1/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	fmt.Println("Commencing countdown.")
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) //read a single byte
+		abort <- struct{}{}
+	}()
 	tick := time.Tick(1 * time.Second)
 	for countdown := 10; countdown > 0; countdown-- {
 		fmt.Println(countdown)
 		<-tick
 	}
+	launch()
+}
+
+func launch() {
+	fmt.Println("Launching!")
 }
