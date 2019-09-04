ff74135864f609bee568ff8cdb04b82bf0c099c7
nick <nicholasjdaniel@gmail.com>
2019-09-04 17:36:59 -0400

    add handleConn goroutine

--- gopl.io/ch8/chat/main.go
+++ gopl.io/ch8/chat/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
 )
@@ -45,5 +47,31 @@ func broadcaster() {
 			clients[cli] = false
 			close(cli)
 		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string) //outgoing client messages
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+
+	leaving <- ch
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) //ignoring network errors
 	}
 }
