dba415d47ca42d9ad1ed4cec838ea23754845cdb
nick <nicholasjdaniel@gmail.com>
2019-09-06 15:42:21 -0400

    add withdraw func

--- gopl.io/ch9/bank1/bank.go
+++ gopl.io/ch9/bank1/bank.go
@@ -2,8 +2,11 @@
 // This confines the shared variable to one goroutine and brokers the access with channel requests
 package bank
 
-var deposits = make(chan int) //send amount to deposit
-var balances = make(chan int) //receive balance
+import "golang.org/x/text/currency"
+
+var deposits = make(chan int)  //send amount to deposit
+var balances = make(chan int)  //receive balance
+var withdraws = make(chan int) //send amount to withdraw
 
 func Deposit(amount int) {
 	deposits <- amount
@@ -13,12 +16,23 @@ func Balance() int {
 	return <-balances
 }
 
+func Withdraw(amount int) bool {
+	x := <-balances
+	if x >= amount {
+		withdraws <- amount
+		return true
+	}
+	return false
+}
+
 func teller() {
 	var balance int
 	for {
 		select {
 		case amount := <-deposits:
 			balance += amount
+		case amount := <-withdraws:
+			balance -= amount
 		case balances <- balance:
 		}
 	}
@@ -27,3 +41,5 @@ func teller() {
 func init() {
 	go teller() //monitor goroutine, brokers access to confined variables using channel requests
 }
+
+//ex9.1 add Withdraw func
