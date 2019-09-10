6a648a8ace22a15a4c6d1ffb432d1aef624c4501
nick <nicholasjdaniel@gmail.com>
2019-09-10 18:20:28 -0400

    add notes and example

---
+++ gopl.io/ch9/memo1/memo.go
@@ -0,0 +1,9 @@
+package memo
+
+//building a concurrent non-blocking cache from 9.7
+//memoizing a function, caching the result of a function, so its only computed once
+//concurrent-safe and avoid contention with single lock designs
+
+func httpGetBody(url string) (interface{}, error) {
+
+}
