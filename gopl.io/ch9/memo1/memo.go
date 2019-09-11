0d4df442e106ac510eaed5659bd4a6c8860a2ed7
nick <nicholasjdaniel@gmail.com>
2019-09-11 17:52:11 -0400

    add memo example

--- gopl.io/ch9/memo1/memo.go
+++ gopl.io/ch9/memo1/memo.go
@@ -1,9 +1,46 @@
 package memo
 
+import (
+	"io/ioutil"
+	"net/http"
+)
+
 //building a concurrent non-blocking cache from 9.7
 //memoizing a function, caching the result of a function, so its only computed once
 //concurrent-safe and avoid contention with single lock designs
 
-func httpGetBody(url string) (interface{}, error) {
+type result struct {
+	value interface{}
+	err   error
+}
 
+//Func is the type of the function to memoize
+type Func func(key string) (interface{}, error)
+
+type Memo struct {
+	f     Func
+	cache map[string]result
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]result)}
+}
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+//NOTE: not concurrency-safe
+func (memo *Memo) Get(key string) (interface{}, error) {
+	res, ok := memo.cache[key]
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.cache[key] = res
+	}
+	return res.value, res.err
 }
