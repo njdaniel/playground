7aea6193223d30f74d92d902b887a7b1a177e3fc
nicholasdaniel <nicholasjdaniel@gmail.com>
2019-10-03 21:41:21 -0400

    recursive depth search for sum

--- leetcode/rangeSumBST/main.go
+++ leetcode/rangeSumBST/main.go
@@ -20,5 +20,19 @@ type TreeNode struct {
 }
 
 func rangeSumBST(root *TreeNode, L int, R int) int {
-
+	sum := 0
+	var f func(node *TreeNode)
+	f = func(node *TreeNode) {
+		if node.Val >= L && node.Val <= R {
+			sum += node.Val
+		}
+		if node.Left != nil {
+			f(node.Left)
+		}
+		if node.Right != nil {
+			f(node.Right)
+		}
+	}
+	f(root)
+	return sum
 }
