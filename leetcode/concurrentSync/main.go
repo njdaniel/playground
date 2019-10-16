d68ce74f94e6f4bcc01a9c5f98104be1075ee2ad
nick <nick@pop-os.localdomain>
2019-10-15 21:43:17 -0400

    pass channel with done

--- leetcode/concurrentSync/main.go
+++ leetcode/concurrentSync/main.go
@@ -10,27 +10,31 @@ func main() {
 type Foo struct {
 	order []int
 }
-func (f *Foo) first() {
+func (f *Foo) first(done chan struct{}) {
 	fmt.Print("first")
+	done <- struct {}{}
 }
-func (f *Foo) second() {
+func (f *Foo) second(done chan struct{}) {
 	fmt.Print("second")
+	done <- struct{}{}
 }
-func (f *Foo) third() {
+func (f *Foo) third(done chan struct{}) {
 	fmt.Print("third")
+	done <- struct{}{}
 }
 //NewFoo is a constructor for a new object Foo
 //takes input of slice of ints
 func NewFoo(order []int) *Foo {
 	f := &Foo{order}
+	done := make(chan struct{})
 	for _, o := range f.order {
 		switch o {
 		case 1:
-			go f.first()
+			go f.first(done)
 		case 2:
-			go f.second()
+			go f.second(done)
 		case 3:
-			go f.third()
+			go f.third(done)
 		}
 	}
 	return f
