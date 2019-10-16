a2945451fa82766976f55ebd72a2059601463fe5
nick <nick@pop-os.localdomain>
2019-10-15 22:30:37 -0400

    debugging

--- leetcode/concurrentSync/main.go
+++ leetcode/concurrentSync/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	
+	NewFoo([]int{1,2,3})
 }
 
 //Foo public object
@@ -25,8 +25,10 @@ func (f *Foo) third(done chan struct{}) {
 //NewFoo is a constructor for a new object Foo
 //takes input of slice of ints
 func NewFoo(order []int) *Foo {
+	fmt.Println("Creating foo object")
 	f := &Foo{order}
 	done := make(chan struct{})
+	fmt.Printf("Len: %d", len(f.order))
 	for _, o := range f.order {
 		switch o {
 		case 1:
