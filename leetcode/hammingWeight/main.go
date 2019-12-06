afa464013a182c984858bc33796f0e1371d80aec
njdaniel <nicholasjdaniel@gmail.com>
2019-12-05 19:29:33 -0500

    add closure to optimize with many calls

--- leetcode/hammingWeight/main.go
+++ leetcode/hammingWeight/main.go
@@ -25,6 +25,21 @@ func hammingWeight(num uint32) int {
 }
 
 //if called many times, use a closure to store the num as a key
-//func hammingWeightOpt(num uint32) int {
-//
-//}
+func hammingWeightOpt(num uint32) int {
+	m := make(map[uint32]int)
+	var f = func() int {
+		if _, ok := m[num]; ok {
+			return m[num]
+		}
+		sum := 0
+		numstr := strconv.FormatUint(uint64(num), 2)
+		for _, v := range numstr {
+			if v == '1' {
+				sum++
+			}
+		}
+		m[num] = sum
+		return sum
+	}
+	return f()
+}
