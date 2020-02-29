49b890c9f2c076195897cb1f55cca8d595fd5506
njdaniel <nicholasjdaniel@gmail.com>
2020-02-28 21:39:56 -0500

    plan for 1337

--- leetcode/1337-kWeakRow/main.go
+++ leetcode/1337-kWeakRow/main.go
@@ -6,5 +6,11 @@ func main() {
 
 
 func kWeakestRows(mat [][]int, k int) []int {
+	rowPower := make(map[int]int, 0)
+	// go through mat and find power of each row
+	// O(NM)
 
+	// slice of rows strength in order
+	// O(N) can be optimized to just to k.. maybe
+	// return [:k]
 }
