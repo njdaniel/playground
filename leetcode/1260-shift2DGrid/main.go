efa20341f0e24e6e977660b31c4ad8a1819cbcdb
njdaniel <nicholasjdaniel@gmail.com>
2020-03-05 13:38:14 -0500

    add shift2Dgrid problem, runtime err slice out of bounds

---
+++ leetcode/1260-shift2DGrid/main.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+	grid :=
+}
+
+func shiftGrid(grid [][]int, k int) [][]int {
+	// plan:
+	// data struct []int
+	// convert grid to []int
+	// put end of []int k values to temp
+	// put at beginning
+	// convert back to [][]int grid
+	m := len(grid)
+	var n int
+	s := make([]int, 0)
+	for _, row := range grid {
+		n = len(row)
+		s = append(s, row...)
+	}
+	//tmp := make([]int, 0)
+	tmp := s[len(s)-k:]
+	s = s[:-k]
+	s = append(tmp, s...)
+	newGrid := make([][]int, 0)
+	for x := 0; x < n; x++ {
+		tmp := s[:m]
+		newGrid = append(newGrid, tmp)
+		s = s[m:]
+	}
+	return newGrid
+}
