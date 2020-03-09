6a35c9d3241ccdad07ffe1ea1a8e75b7c26881fb
njdaniel <nicholasjdaniel@gmail.com>
2020-03-09 12:35:08 -0400

    fix solution, timeout err

--- hr/newYearChaos/main.go
+++ hr/newYearChaos/main.go
@@ -9,13 +9,18 @@ func main() {
 func minimumBrides(q []int32)  {
 	bribes := 0
 	for k, v := range q {
-		if k+3 >= int(v) {
+		if k+3 < int(v) {
 			fmt.Println("Too chaotic")
 			return
 		}
-		if k+1 > int(v) {
-			bribes += ((k+1)-int(v))
+		// for each value that's higher in front, increase the bribe
+		// counting how much each value gets bribed
+		for i := k-1; i >= 0; i-- {
+			if q[i] > v {
+				bribes++
+			}
 		}
 	}
+
 	fmt.Println(bribes)
-}
+ }
