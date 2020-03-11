95972e8ef625cdbe4882b1c851078dfafdfdf21d
njdaniel <nicholasjdaniel@gmail.com>
2020-03-10 21:20:48 -0400

    []rune check palindrome

--- leetcode/palindrome/palindrome/palindrome.go
+++ leetcode/palindrome/palindrome/palindrome.go
@@ -1,11 +1,17 @@
 package palindrome
 
-import "strconv"
+import (
+	"reflect"
+	"strconv"
+)
 
 // Goal:
 // Use polymorphism / generics / substitute functions for different types for a function
 // Given an array of different possible types (ex, int, rune)
 // ..probably should just use rune. Convert any number, string, to []rune for this case.
+// otherwise, the interface would need to use reflection to determine the type and changed to
+// []rune anyways.
+// still might be nice to use interface to abstract the types for determining if its a palindrome
 
 type Palindrome interface {
 	IsPalindrome() bool
@@ -13,7 +19,7 @@ type Palindrome interface {
 
 
 
-func IsPalindrome(x int) bool {
+func IsPalindromeOriginal(x int) bool {
 	s := strconv.Itoa(x)
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
@@ -21,6 +27,18 @@ func IsPalindrome(x int) bool {
 	}
 	reverse := string(runes)
 	if s == reverse {
+		return true
+	}
+	return false
+}
+
+func IsPalindrome(runes []rune) bool {
+	reverse := runes
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		reverse[i], reverse[j] = reverse[j], reverse[i]
+	}
+	// compare two arrays?
+	if reflect.DeepEqual(runes, reverse) {
 		return true
 	}
 	return false
---
+++ leetcode/palindrome/palindrome/palindrome_test.go
@@ -0,0 +1,26 @@
+package palindrome
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+	type args struct {
+		runes []rune
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "letter test true", args:args{runes: []rune{'a', 'b', 'b', 'a'}}, want:true},
+		{name: "letter test false", args:args{runes: []rune{'a', 'a', 'b'}}, want:false},
+		{name: "number test true", args:args{runes: []rune{'1', '2', '1'}}, want:true},
+		{name: "number test false", args:args{runes: []rune{'1', '2', '3'}}, want:false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsPalindrome(tt.args.runes); got != tt.want {
+				t.Errorf("IsPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
