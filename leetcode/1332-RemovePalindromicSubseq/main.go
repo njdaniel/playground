518edbfb3ad15a8b89170243b421ec6e97ad6250
njdaniel <nicholasjdaniel@gmail.com>
2020-03-13 11:11:52 -0400

    check for palindrome

--- leetcode/1332-RemovePalindromicSubseq/main.go
+++ leetcode/1332-RemovePalindromicSubseq/main.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"playground/leetcode/palindrome/palindrome"
+)
+
 func main() {
-	
+	s := "abb"
+	fmt.Println(removePalindromeSub(s))
 }
 
 func removePalindromeSub(s string) int {
-
+	pals := 0
+	for x := len(s); x>0; x-- {
+		 if palindrome.IsPalindrome([]rune(s[:x])) {
+		 	pals++
+		 	s = s[x:]
+		 	x = len(s)
+		 }
+	}
+	return pals
 }
