527a034a127671463bbb28c142ba661ba98e3161
nick <nick@pop-os.localdomain>
2020-03-25 21:06:14 -0400

    open files

--- interview/aggregateLogs/main.go
+++ interview/aggregateLogs/main.go
@@ -9,32 +9,34 @@ import (
 )
 
 func main() {
-	fmt.Println(time.Now())
+	fmt.Println(time.Now().Format("2006-01-02T15:04:05"))
 }
 
 // print out multiple log records from multiple log files in order
 
 //example
 //timestamp msg
+//EDIT: log format to 2006-01-02T15:04:05
 //file1.log
-//2020-03-25 15:18:26.04957 msg 1
-//2020-03-25 15:18:26.04957 msg 2
-//2020-03-25 15:18:26.04957 msg 3
-//2020-03-25 15:18:27.04957 msg 4
+//2020-03-25T15:18:26 msg 1
+//2020-03-25T15:18:26 msg 2
+//2020-03-25T15:18:26 msg 3
+//2020-03-25T15:18:27 msg 4
 
 //file2.log
-//2020-03-25 15:18:26.03957 msg 5
-//2020-03-25 15:18:26.14957 msg 6
-//2020-03-26 15:18:21.04957 msg 7
-//2020-03-26 15:18:26.04958 msg 8
+//2020-03-25T15:18:26 msg 5
+//2020-03-25T15:18:26 msg 6
+//2020-03-26T15:18:21 msg 7
+//2020-03-26T15:18:26 msg 8
 
 //file3.log
-//2020-03-25 14:18:26.04957 msg 9
-//2020-03-25 16:18:26.04957 msg 10
-//2020-03-25 17:18:26.04957 msg 11
-//2020-03-25 17:19:26.04957 msg 12
+//2020-03-25T14:18:26 msg 9
+//2020-03-25T16:18:26 msg 10
+//2020-03-25T17:18:26 msg 11
+//2020-03-25T17:19:26 msg 12
 
 //Result prints
+// TODO: check if results are correct
 // 9
 // 5
 // 1
@@ -60,14 +62,25 @@ type Log struct {
 	msg string
 }
 
+// AggregateLogs prints out the log records in order
 func AggregateLogs(files []string) {
 	logTimes := make([]chan time.Time, len(files))
 	for _, file := range files {
+		//setup goroutine
 		readFile, err := os.Open(file)
 		if err != nil {
 			log.Fatalf("error failed to open file: %v", err)
 		}
 		fileScanner := bufio.NewScanner(readFile)
 		fileScanner.Split(bufio.ScanLines)
+		//get timestamp
+		//send back
+	}
+	//compare all the returned timestamps
+	for i := 1;i > len(logTimes); {
+		for k, v := range logTimes {
+			var oldest time.Time
+
+		}
 	}
 }
