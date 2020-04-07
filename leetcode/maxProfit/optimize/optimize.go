4b144ed94c054c74abb42e0ae8865be78f8e7978
njdaniel <nicholasjdaniel@gmail.com>
2020-04-07 16:53:15 -0400

    optimized maxprofit solution

---
+++ leetcode/maxProfit/optimize/optimize.go
@@ -0,0 +1,12 @@
+package optimize
+
+func MaxProfit(prices []int) int {
+	maxprofit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxprofit += prices[i] - prices[i-1]
+		}
+	}
+	return maxprofit
+}
+
--- leetcode/maxProfit/optimize/optimize_test.go
+++ leetcode/maxProfit/optimize/optimize_test.go
@@ -1,8 +1,8 @@
-package main
+package optimize
 
 import "testing"
 
-func Test_maxProfit(t *testing.T) {
+func TestMaxProfit(t *testing.T) {
 	type args struct {
 		prices []int
 	}
@@ -14,13 +14,13 @@ func Test_maxProfit(t *testing.T) {
 		{name: "test1", args: args{prices: []int{7,1,5,3,6,4}}, want: 7},
 		{name: "test2", args: args{prices: []int{1,2,3,4,5}}, want: 4},
 		{name: "test3", args: args{prices: []int{7,6,5,4,3,2}}, want: 0},
-		//{name: "test4", args: args{prices: []int{6,1,3,2,4,7}}, want: 7},
+		{name: "test4", args: args{prices: []int{6,1,3,2,4,7}}, want: 7},
 		{name: "test5", args: args{prices: []int{6,1,3,2,7}}, want: 7},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := maxProfit(tt.args.prices); got != tt.want {
-				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			if got := MaxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("MaxProfit() = %v, want %v", got, tt.want)
 			}
 		})
 	}
