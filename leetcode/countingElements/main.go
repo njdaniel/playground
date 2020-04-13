c1e6923731a3472b7c2f28650c2014dcff61a7de
njdaniel <nicholasjdaniel@gmail.com>
2020-04-13 14:10:02 -0400

    add counting elements

---
+++ leetcode/countingElements/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	
+}
+
+func countElements(arr []int) int {
+	count := 0
+
+	return count
+}
---
+++ leetcode/countingElements/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func Test_countElements(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countElements(tt.args.arr); got != tt.want {
+				t.Errorf("countElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
