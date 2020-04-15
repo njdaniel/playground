a921a6d9c2e950dd9b7cd96c6f7696d87b79fe0a
njdaniel <nicholasjdaniel@gmail.com>
2020-04-15 12:50:10 -0400

    first countelem attempt

--- leetcode/countingElements/main.go
+++ leetcode/countingElements/main.go
@@ -4,8 +4,17 @@ func main() {
 	
 }
 
+// countElements counts how many consecutive numbers are +1 from the last
 func countElements(arr []int) int {
 	count := 0
+	if len(arr) == 1 {
+		return 0
+	}
+	for i := 1; i < len(arr); i++ {
+		if arr[i] == arr[i-1]+1 {
+			count++
+		}
+	}
 
 	return count
 }
