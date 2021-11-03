be2424d2fe8850a8e65485628eacffd5f7798f79
Nicholas Daniel <nick@pop-os.localdomain>
2021-11-03 11:12:26 -0400

    read bodys

--- requests/main.go
+++ requests/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	url := "https://esi.evetech.net"
-	ancestriesURL := url + "latest/universe/ancestries/?datasource=tranquility&language=en"
-	resp, _ := http.Get(ancestriesURL)
-	fmt.Println(*resp)
+	ancestriesURL := url + "/latest/universe/ancestries/?datasource=tranquility&language=en"
+	resp, err := http.Get(ancestriesURL)
+	if err != nil {
+		fmt.Println("error getting url: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(resp.StatusCode)
+	var body []byte
+	_, err = io.ReadFull(resp.Body, body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Println("error io reading body: ", err)
+	}
+	fmt.Println(body)
 }
