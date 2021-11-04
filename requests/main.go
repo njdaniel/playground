15c1b5924a99bae1d81447298d2e28c6085df45e
Nicholas Daniel <nick@pop-os.localdomain>
2021-11-04 17:43:44 -0400

    get auth

--- requests/main.go
+++ requests/main.go
@@ -4,11 +4,29 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
+	getAncestries()
+}
 
+func secureToken(user, secret string) {
+	authorizationURL := "https://login.eveonline.com/v2/oauth/authorize"
+	v := url.Values{}
+	v.Set("user", user)
+	v.Set("password", secret)
+	resp, err := http.PostForm(authorizationURL, v)
+	if err != nil {
+		fmt.Println("error authorizing: ", err)
+	}
+	if resp.StatusCode > 299 {
+		fmt.Println("error status code: ", resp.Status)
+	}
+}
+
+func getAncestries() {
 	url := "https://esi.evetech.net"
 	ancestriesURL := url + "/latest/universe/ancestries/?datasource=tranquility&language=en"
 	resp, err := http.Get(ancestriesURL)
@@ -23,9 +41,4 @@ func main() {
 		fmt.Println("error reading response body", err)
 	}
 	fmt.Println(string(body))
-}
-
-func SecureToken() {
-	AuthorizationURL := "https://login.eveonline.com/v2/oauth/token"
-	resp, err := http.PostForm()
 }
