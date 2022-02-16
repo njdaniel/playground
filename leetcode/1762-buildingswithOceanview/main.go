a57c7259d44c571cf69ad3a65c3c1c06ee2ece5c
njdaniel <nicholasjdaniel@gmail.com>
2022-02-16 10:29:00 -0500

    complete leetcode 1762

--- leetcode/1762-buildingswithOceanview/main.go
+++ leetcode/1762-buildingswithOceanview/main.go
@@ -12,6 +12,18 @@ func findBuildings(heights []int) []int {
 	buildingswithOceanview := make([]int, 0)
 	//bruteforce: loop through each input and compare to the rest of slice to the right
 	//  if its > all then add to list and return
+	for i, v := range heights {
+		oceanView := true
+		for j := i + 1; j < len(heights); j++ {
+			if v <= heights[j] {
+				oceanView = false
+				break
+			}
+		}
+		if oceanView {
+			buildingswithOceanview = append(buildingswithOceanview, i)
+		}
+	}
 
 	return buildingswithOceanview
 }
--- leetcode/1762-buildingswithOceanview/main_test.go
+++ leetcode/1762-buildingswithOceanview/main_test.go
@@ -16,6 +16,7 @@ func Test_findBuildings(t *testing.T) {
 	}{
 		{name: "example1", args: args{heights: []int{4, 2, 3, 1}}, want: []int{0, 2, 3}},
 		{name: "example2", args: args{heights: []int{4, 3, 2, 1}}, want: []int{0, 1, 2, 3}},
+		{name: "example3", args: args{heights: []int{1, 3, 2, 4}}, want: []int{3}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
