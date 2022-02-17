34e5499636f31234f4760063c172a66a9227549b
njdaniel <nicholasjdaniel@gmail.com>
2022-02-17 08:52:17 -0500

    update leetcode 394

--- leetcode/394-decodeString/main.go
+++ leetcode/394-decodeString/main.go
@@ -1,7 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
+func main() {
+	s := "3[a]2[bc]"
+	fmt.Println(decodeString(s))
 }
 
 //topics: string, stack, recursion
@@ -20,33 +27,61 @@ func decodeString(s string) string {
 	//if yes return the result if not find the next part of the pattern
 	ra := []rune(s)
 	//raTemp := make([]rune, 0)
-	var pend int
-	var pbegin int
-	for i, v := range ra {
+
+	//check if only one bracket set
+	numbrackets := 0
+	for _, v := range ra {
 		if v == ']' {
-			pend = i
-			//find the beginning of the k[encode](pbegin)
-			for j := i - 1; j >= 0; j-- {
-				if isNumber(ra[j]) {
-					for k := j - 1; k >= 0; k-- {
-						if !isNumber(ra[k]) {
-							pbegin = k + 1
-							break
-						}
-						pbegin = 0
-					}
-				}
-			}
-			pend = i + 1
-			raTemp := ra[pbegin:pend]
-			decodedString := decodeString(string(raTemp))
-			//cut + replace
-			ra = append(ra[:pbegin], ra[pend-1:]...)
-			ra = append(ra[:pbegin], append([]rune(decodedString), ra[pbegin:]...)...)
-			//break
-			i = 0
+			numbrackets++
 		}
 	}
+	if numbrackets == 1 {
+		//k := 0
+		ks := make([]rune, 0)
+		es := ""
+		p := 0
+		for i, v := range ra {
+			if v == '[' {
+				p = i
+				break
+			}
+			ks = append(ks, v)
+		}
+		k, err := strconv.Atoi(string(ks))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+
+		var pend int
+		var pbegin int
+		for i, v := range ra {
+			if v == ']' {
+				pend = i
+				//find the beginning of the k[encode](pbegin)
+				for j := i - 1; j >= 0; j-- {
+					if isNumber(ra[j]) {
+						for k := j - 1; k >= 0; k-- {
+							if !isNumber(ra[k]) {
+								pbegin = k + 1
+								break
+							}
+							pbegin = 0
+						}
+					}
+				}
+				pend = i + 1
+				raTemp := ra[pbegin:pend]
+				decodedString := decodeString(string(raTemp))
+				//cut + replace
+				ra = append(ra[:pbegin], ra[pend-1:]...)
+				ra = append(ra[:pbegin], append([]rune(decodedString), ra[pbegin:]...)...)
+				//break
+				i = 0
+			}
+		}
+	}
+
 	//remove and replace
 	return string(ra)
 }
