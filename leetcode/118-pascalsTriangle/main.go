ab6602935f8db7f0f22ecd9296707bd617e490ec
njdaniel <nicholasjdaniel@gmail.com>
2022-09-09 15:46:17 -0400

    add pascals triangle

---
+++ leetcode/118-pascalsTriangle/main.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+
+}
+
+// generate creates a pascal triangle given the number of rows
+//constraint: 1<=numRows<=30
+func generate(numRows int) [][]int {
+	rows := make([][]int, 0)
+	if numRows < 1 {
+		return rows
+	}
+	rows = append(rows, []int{1})
+
+	for i := 1; i < numRows; i++ {
+		newRow := make([]int, 0)
+		newRow = append(newRow, 1)
+		for j := 1; j < len(rows[i-1]); j++ {
+			genNum := rows[i][j-1] + rows[i][j]
+			newRow = append(newRow, genNum)
+		}
+		newRow = append(newRow, 1)
+	}
+
+	return rows
+}
